60c19360344cf3305b3f5f9e80a3288aab8beaf6
des733050-png <des733050@gmail.com>
2026-04-20 09:32:18 +0300

    Configure WSGI with environment variables
    Added environment variables for Django settings and security.

--- backend/backend/config/wsgi.py
+++ backend/backend/config/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = "/home/vsifdd/gone/backend/backend"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+os.environ["DJANGO_DEBUG"] = "False"
+os.environ["DJANGO_ALLOWED_HOSTS"] = "vsifdd.pythonanywhere.com,.vercel.app,127.0.0.1,localhost"
+os.environ["DJANGO_CSRF_TRUSTED_ORIGINS"] = "https://vsifdd.pythonanywhere.com,https://*.vercel.app"
+os.environ["DJANGO_CORS_ALLOWED_ORIGINS"] = "https://patient-rosy.vercel.app,https://gone-gules.vercel.app"
+os.environ["DJANGO_SECRET_KEY"] = "django-insecure-x3__fb505knw7r+uj_mr1^1n!m#1)=*v0zbkgpwj7c2@ye#2n="
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
